92ceb5b826b6341283ca92fab5b8494a1382809a
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-06-23 23:37:36 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Another comment:
-package {'puppet-lint':
-  ensure   => '2.1.1',
-  provider => gem,
+# installs the package puppet-lint
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
