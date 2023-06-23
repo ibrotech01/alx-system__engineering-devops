5f34fd65b66a5aa0addc21168df0f43bb368e033
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-06-23 23:38:42 +0100

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-# Another comment:
-exec {'killmenow':
-  command => 'pkill killmenow',
+# create a manifest that kills a process named killmenow
+# using pkill with puppet
+
+exec { 'pkill':
+  command => 'pkill -f killmenow',
   path    => '/usr/bin',
 }
